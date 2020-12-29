ba77fe0c76e1dcf0bf2708cb749274b5105d833b
Javier Moreno <mq2880@gmail.com>
2020-12-29 18:39:38 +0100

    commit

---
+++ AzureStorageDemo/Startup.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AzureStorageDemo.Data;
+using AzureStorageDemo.Middleware;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Azure;
+using Azure.Storage.Queues;
+using Azure.Storage.Blobs;
+using Azure.Core.Extensions;
+
+namespace AzureStorageDemo
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        private IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            services.AddDbContext<PhotoContext>(options =>
+                 options.UseSqlite("Data Source=photoDb.db"));
+
+            /*services.AddAzureClients(builder =>
+            {
+                builder.AddBlobServiceClient(Configuration["ConnectionStrings:AzureStorageConnectionString-1:blob"], preferMsi: true);
+                builder.AddQueueServiceClient(Configuration["ConnectionStrings:AzureStorageConnectionString-1:queue"], preferMsi: true);
+            });*/
+        }
+
+        public void Configure(IApplicationBuilder app, PhotoContext photoContext, IHostingEnvironment environment)
+        {
+            photoContext.Database.EnsureDeleted();
+            photoContext.Database.EnsureCreated();
+
+            app.UseStaticFiles();
+
+            app.UseNodeModules(environment.ContentRootPath);
+
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+    internal static class StartupExtensions
+    {
+        public static IAzureClientBuilder<BlobServiceClient, BlobClientOptions> AddBlobServiceClient(this AzureClientFactoryBuilder builder, string serviceUriOrConnectionString, bool preferMsi)
+        {
+            if (preferMsi && Uri.TryCreate(serviceUriOrConnectionString, UriKind.Absolute, out Uri serviceUri))
+            {
+                return builder.AddBlobServiceClient(serviceUri);
+            }
+            else
+            {
+                return builder.AddBlobServiceClient(serviceUriOrConnectionString);
+            }
+        }
+        public static IAzureClientBuilder<QueueServiceClient, QueueClientOptions> AddQueueServiceClient(this AzureClientFactoryBuilder builder, string serviceUriOrConnectionString, bool preferMsi)
+        {
+            if (preferMsi && Uri.TryCreate(serviceUriOrConnectionString, UriKind.Absolute, out Uri serviceUri))
+            {
+                return builder.AddQueueServiceClient(serviceUri);
+            }
+            else
+            {
+                return builder.AddQueueServiceClient(serviceUriOrConnectionString);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,67 @@
-# 20486C-Mod14-Demo-Lesson3
+# Tareas del MOC
+
+## Module 14: Hosting and Deployment
+
+# Lesson 3: Microsoft Azure Fundamentals
+
+### Demonstration: How to Upload an Image to Microsoft Azure Blob Storage
+
+- Creamos un nueva cuenta de almacenamiento en Azure
+
+  
+
+  ![](./img/Captura1.jpg)
+
+  
+
+  
+
+  ![](./img/Captura2.jpg)
+
+  
+
+- Añadimos un nuevo contenedor
+
+- Pulsamos en crear
+
+
+
+![](./img/Captura3.jpg)
+
+
+
+- Subimos una imagen
+
+
+
+![](./img/Captura4.jpg)
+
+
+
+- Comprobamos lo que ha creado
+- En el proyecto añadimos el servicio de almacenamiento
+
+![](./img/Captura7.jpg)
+
+- 
+
+- Modificamos el controlador para que acepte ese servicio
+
+- Ejecutamos la aplicacion y añadimos una foto
+
+  
+
+  
+
+![](./img/Captura5.jpg)
+
+
+
+- comprobamos que ha subido
+
+  
+
+![](./img/Captura6.jpg)
+
+
+
